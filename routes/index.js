0ace7ed8664049f85ddcfd13271d53bd22919964
Susi Mehl <susimehl@gmx.de>
2023-02-15 15:44:14 +0100

    deleted loggedin

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { isLoggedIn } = require("../middleware/route-guard")
+
 
 /* GET home page */
 
-router.get("/", (req, res, next) => {
-    res.render("index")
-  })
-  
-  router.get("/main", isLoggedIn, (req, res, next) => {
-    const loggedInUser = req.session.user
-    res.render("main", { user: loggedInUser })
-  })
-  
-  router.get("/private", isLoggedIn, (req, res, next) => {
-    const loggedInUser = req.session.user
-    res.render("private", { user: loggedInUser })
-  })
+
   
 
 module.exports = router;
