29e39ae4db8b1ff35eb307b89f993974937acc67
Susi Mehl <susimehl@gmx.de>
2023-02-15 15:42:42 +0100

    added loggedIn to index.js route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@ const router = express.Router();
 
 /* GET home page */
 
-
+router.get("/", (req, res, next) => {
+    res.render("index")
+  })
+  
+  router.get("/main", isLoggedIn, (req, res, next) => {
+    const loggedInUser = req.session.user
+    res.render("main", { user: loggedInUser })
+  })
+  
+  router.get("/private", isLoggedIn, (req, res, next) => {
+    const loggedInUser = req.session.user
+    res.render("private", { user: loggedInUser })
+  })
+  
 
 module.exports = router;
