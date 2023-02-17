25b1fac3f3db25783d43b00211e8de45aeedbfd2
Nasim ghalebi <n.ghalebi@gmail.com>
2023-02-17 10:37:41 +0100

    add style, remove button, version 1

--- routes/cart.js
+++ routes/cart.js
@@ -7,12 +7,24 @@ const {isLoggedIn}= require("../middleware/route-guard")
 
 
 
-router.get("/cart",isLoggedIn, ((req, res) =>{
+router.get("/cart",isLoggedIn, ((req, res, next) =>{
    let id = req.session.user._id
    Cart.findOne({userId: id})
    .then(cartFromUser=> {
-       res.render("cart", {cartItems: cartFromUser.products})
+   
+    if(cartFromUser != null){
+        
+        res.render("cart", {cartItems: cartFromUser.products})
+        console.log('ID HERE BELOW!!!!!!!!')
+        console.log(cartFromUser.products)
+
+    } else {
+        res.render("cart", {cartItems:{}})
+
+    }
+       
    })
+   .catch(err => next(err)) 
 })
 )
 
@@ -22,7 +34,8 @@ router.post("/addtocart/:id", isLoggedIn , (req, res, next) =>{
     console.log('here')
     const {id} = req.params
     const userId = req.session.user._id
-
+    console.log(id)
+    console.log(userId)
     Product.findById(id)
     .then((product)=> {
         Cart.findOneAndUpdate({ userId: userId }, {$push:{ products: {productId: id, quantity: 1, price:product.price }}},{new:true})
@@ -35,7 +48,26 @@ router.post("/addtocart/:id", isLoggedIn , (req, res, next) =>{
     })
 })
 
+router.post("/cart/:id", isLoggedIn, ((req, res, next) => {
+    console.log("oooooooooooooooooooooooooooooooo")
+    let usrId = req.session.user._id
+    const { prdId } = req.params
+    console.log('cart here product id!!!!')
+    console.log(prdId)
+    Cart.findOneAndUpdate({ userId: usrId }, {$pull:{productId: prdId}})
 
+            // const prd = cartFromUser.products.productId(prdId)
+            // prd.remove()
+            // cartFromUser.save()
+                .then(updatedCart => {
+                    res.redirect("/cart")
+                    // console.log(updatedCart)
+                })
+                .catch(err => next(err))
+
+        })
+
+)
 
 
 
